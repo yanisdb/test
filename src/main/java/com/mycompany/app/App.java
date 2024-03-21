76e4540f6dba7743a916937a4d283bcaf0d0bffb
Yanis De Busschere <yanisdebusschere@gmail.com>
2024-03-21 15:59:05 +0100

    Adds infinite loop (#4)

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,25 +1,13 @@
 package com.mycompany.app;
 
-import java.io.FileInputStream;
-import java.io.ObjectInputStream;
-
 public class App {
 
     public static void main(String[] args) {
-        // The file 'example.obj' is assumed to contain a serialized Java object
-        String filename = "example.obj";
 
-        try (FileInputStream fileInputStream = new FileInputStream(filename);
-             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
-
-            // Deserializing object without any validation
-            Object obj = objectInputStream.readObject();
-
-            // Normally, you would do something with the object here
-            System.out.println("Deserialized object: " + obj);
-
-        } catch (Exception e) {
-            e.printStackTrace();
+        int i = 0;
+        while(true) {
+            System.out.println("Hello World!");
+            i++;
         }
     }
 }
